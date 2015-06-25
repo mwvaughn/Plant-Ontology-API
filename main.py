c019bfbac797d6926411dfd0dd460071483685f3
Matthew Vaughn <vaughn@tacc.utexas.edu>
2015-06-25 15:27:25 -0500

    Updates to support latest ADAMA features including documented endpoints, icon, provenance, and exception handling

--- main.py
+++ main.py
@@ -3,30 +3,25 @@ import re
 import json
 
 def search(arg):
-    
+
 # arg contains a dict with a single key:value
 # term is PO accession term and is mandatory
-    
-    # Return nothing if client didn't pass in a term parameter
-    if not ('term' in arg):
-       return
-    
+
     # Validate against a regular expression
     term = arg['term']
     #term = term.upper()
     p = re.compile('[A-Z,a-z,0-9]', re.IGNORECASE)
     if not p.search(term):
-       return
+        raise Exception('Invalid search term')
 
-    r = requests.get('http://palea.cgrb.oregonstate.edu/services/PO_web_service.php?request_type=term_search&search_value=' + term + '&inc_synonyms&branch_filter=plant_anatomy&max=20&prioritize_exact_match')
-    
-    #print r.headers['Content-Type']
-    #print r.text
-    
-    if r.ok:
+    r = requests.get('http://iplant.plantontology.org/services/PO_web_service.php?request_type=term_search&search_value=' + term + '&inc_synonyms&branch_filter=plant_anatomy&max=20&prioritize_exact_match')
+
+    # Raise exception on bad HTTP status
+    r.raise_for_status()
+    try:
        return r.headers['Content-Type'], r.content
-    else:
-      return 'text/plaintext; charset=UTF-8', 'An error occurred on the remote server'
+    except ValueError:
+        raise Exception('Undefined error: '.format(response.text))
 
 def list(arg):
-    pass
+    raise Exception('Not implemented yet')
